91b2eca35c7f637f152253f37897d665740d834e
zigfridExe <zigfrid.exe@gmail.com>
2020-09-08 19:36:28 -0300

    Pasta de Aula para python

---
+++ Python/AulasPython/Aula06.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um outro valor'))
+print(type(n1))
---
+++ Python/Execicios/ex003.py
@@ -0,0 +1,3 @@
+v1 = int(input('Digite o primeiro valor: '))
+v2 = int(input('Digite o segundo valor: '))
+print('A soma de ', v1, "mais", v2, 'é', v1 + v2)
