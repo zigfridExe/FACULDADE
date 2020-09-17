e768ca50d4b30824bec2f500c865111f4b1393e2
zigfridExe <zigfrid.exe@gmail.com>
2020-09-17 15:18:43 -0300

    declaração de vareaveis no Python

--- Python/AulasPython/Aula007.py
+++ Python/AulasPython/Aula007.py
@@ -1,3 +1,12 @@
-# aula sobre operações aritméticas
-(10+10)
-10**10
+print('Hello, what is your language? ')
+lingua = input('Qual o seu Idioma? ')
+if lingua == 'Português':
+    nome = input('Qual o seu nome? ')
+    idade = input('Qual a sua idade? ')
+    print('Bem vindo {} sua idade é {}'.format(nome, idade))
+elif lingua == 'English':
+    nome2 = input('Whats your name? ')
+    idade2 = input('How old are you? ')
+    print('Welcome {} your age is {}'.format(nome2, idade2))
+else:
+    print('ERRO 404')
--- Python/AulasPython/Faculdade.py
+++ Python/AulasPython/Faculdade.py
@@ -1,17 +1,10 @@
 salario = int(input('Qual o seu salário: '))
+print('Ganha bem em filho!!')
 imposto = input('Quanto de imposto vc pagar: ')
+print('Xiii ta fudido')
 if not imposto:
     imposto = 27.5
 else:
     imposto = float(imposto)
 sal = salario - (salario * (imposto*0.01))
 print(sal)
-
-salario = int(input('Qual o seu salario: '))
-imposto = input('Quanto de imposto: ')
-if not imposto:
-    imposto = 27.5
-else:
-    imposto = float(imposto)
-sal = salario - (salario * imposto) / imposto
-print(sal, salario, imposto)
