17b3115196c6560c6c77070decfdfa1dbd950fb4
zigfridExe <zigfrid.exe@gmail.com>
2020-09-19 20:35:18 -0300

    Função com parametro nao funcionou

---
+++ Python/AulasPython/Faculdade/TA1/04_Funcoes_Parametro_TA1.py
@@ -0,0 +1,7 @@
+def lerinteiro():
+    n = int(input('Qual o numero inteiro: '))
+    return n
+
+
+x = lerinteiro()
+print('Valor lido na função = {}'.format(x))
---
+++ Python/AulasPython/Faculdade/TA1/05_Funcoes_com_Parametro_TA1.py
@@ -0,0 +1,12 @@
+# isso nao funcionou
+
+def soma(x, y=1):
+    r = x + y
+    return r
+
+
+print('Ínicio do programa')
+a = int(input('Digite um valor para A: '))
+b = int(input('Digite um valor para B: '))
+s = soma(a, b)
+print('o valor é {}'.format(s))
