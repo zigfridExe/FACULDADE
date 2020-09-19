6869360680a07ee43cc9575abc304c08006418d9
zigfridExe <zigfrid.exe@gmail.com>
2020-09-19 14:43:58 -0300

    Scrip python basico escopo de funções

--- Python/AulasPython/Faculdade/TA1/03_Escopo_funcoes_TA1.py
+++ Python/AulasPython/Faculdade/TA1/03_Escopo_funcoes_TA1.py
@@ -1,5 +1,11 @@
 # Utilização de escopo de vareaveis
 # no Python
 def EstudaEscopo():
-    y = X * 2
-    print('X Global')
+    Y = X * 2
+    print('X Global existe DENTRO da funcao: valor = {}'.format(X))
+    print('Y local existe DENTRO da funcao: valor = {}'.format(Y))
+print('Inicio do Programa')
+X = 10
+print('X Global existe FORA da funcao: valor = {}'.format(X))
+EstudaEscopo()
+print('Fim do Programa')
