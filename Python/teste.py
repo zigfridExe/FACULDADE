ab86c13a629c24c42761de881df4800fdaa484f0
zigfridExe <zigfrid.exe@gmail.com>
2020-09-23 19:34:31 -0300

    Novo projeto
    novo projeto no pycharm consegui recuperar fazendo um novo projeto, e agregando as pastas que tinham o exercicio

--- Python/teste.py
+++ Python/teste.py
@@ -1,6 +1,1 @@
-login = input("Login: ")
-senha = input("Senha: ")
-if login == "userpy" and senha == "teste123":
-    print("Bem-vindo {}".format(login))
-else:
-    print("O login {} falhou, verifique sua senha {} e tente novamente".format(login, senha))
+nota1 = 8.75
